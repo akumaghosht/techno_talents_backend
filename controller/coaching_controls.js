c6ec3a828499d2c94de3eed93e108638eeba2a09
akumaghosht <tathagataghosh0111@gmail.com>
2025-09-23 01:58:26 +0530

    there was one error in previous commit

--- controller/coaching_controls.js
+++ controller/coaching_controls.js
@@ -10,22 +10,22 @@ const get_jee_coaching=async(req,res)=>{
 }
 const get_neet_coachings=async(req,res)=>{
     const neet_coachings=await Coachings.find({coaching_category:"NEET"})
-    res.json()
+    res.json(neet_coachings)
 }
 const get_cat_coachings=async(req,res)=>{
     const cat_coachings=await Coachings.find({coaching_category:"CAT"})
-    res.json()
+    res.json(cat_coachings)
 }
 const get_cuet_coachings=async(req,res)=>{
     const cuet_coachings=await Coachings.find({coaching_category:"CUET"})
-    res.json()
+    res.json(cuet_coachings)
 }
 const get_upsc_coachings=async(req,res)=>{
     const upsc_coachings=await Coachings.find({coaching_category:"UPSC"})
-    res.json()
+    res.json(upsc_coachings)
 }
 const get_gre_gmat_coachings=async(req,res)=>{
     const gre_gmat_coachings=await Coachings.find({coaching_category:"GRE/GMAT"})
-    res.json()
+    res.json(gre_gmat_coachings)
 }
 module.exports={get_coachings,get_jee_coaching,get_neet_coachings,get_cat_coachings,get_cuet_coachings,get_upsc_coachings,get_gre_gmat_coachings}
